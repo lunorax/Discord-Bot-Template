e1db369454e24f6ed395cbad5990a1c671ee3ba1
Luno <lunorax09@gmail.com>
2026-03-19 12:36:10 +0100

    Update bot.py

--- Main/bot.py
+++ Main/bot.py
@@ -1,1 +1,15 @@
 import discord
+from discord.ext import commands
+from discord import app_commands
+import os
+import config
+
+# intents setup
+intents = discord.Intents.default()
+intents.message_content = True
+
+
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+# Put bot online
+bot.run(config.DCBOTTOKEN)
